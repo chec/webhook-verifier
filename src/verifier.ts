5564e1b0ca3e7f7b9bdab9cc1fa45c46da25b5c7
Robbie Averill <robbie@chec.io>
2021-10-25 14:41:21 -0700

    Add webhook verifier with TS and tests

---
+++ src/verifier.ts
@@ -0,0 +1,46 @@
+/**
+ * Chec webhook verifier
+ *
+ * Copyright (c) 2021, Chec/Commerce.js
+ * Released under the BSD-3-Clause license (see LICENSE.md).
+ */
+
+import { VerificationError } from './errors';
+// @ts-ignore
+import * as crypto from 'crypto';
+
+interface Payload {
+  signature?: string,
+  created: number,
+}
+
+export function verifyWebhook(data: Payload, signingKey: string, maxAgeSeconds: number = 300): void {
+  // Extract the signature
+  const { signature } = data;
+  delete data.signature;
+
+  // Ensure we have a signature
+  if (!signature || !signature.length) {
+    throw new VerificationError('Signature was not found in payload.');
+  }
+
+  // Ensure we have a signing key
+  if (!signingKey || !signingKey.length) {
+    throw new VerificationError('Signing key was not provided.');
+  }
+
+  // Ensure the signature is valid
+  const expectedSignature = crypto.createHmac('sha256', signingKey)
+    .update(JSON.stringify(data))
+    .digest('hex');
+  if (expectedSignature !== signature) {
+    throw new VerificationError('Signature mismatch.');
+  }
+
+  // Check age of request
+  const then = new Date(data.created * 1000);
+  const now = new Date();
+  if (+then < (+now - maxAgeSeconds * 1000)) {
+    throw new VerificationError('Request was made too far in the past.');
+  }
+}
---
+++ tests/verifier.test.ts
@@ -0,0 +1,57 @@
+import { verifyWebhook } from '../src';
+import { VerificationError } from '../src/errors';
+import * as crypto from 'crypto';
+
+describe('verifyWebhook', () => {
+  it('throws an error when the signature is missing', () => {
+    function verify() {
+      verifyWebhook({signature: '', created: 1}, 'bar');
+    }
+    expect(verify).toThrowError(VerificationError);
+    expect(verify).toThrowError('Signature was not found in payload.');
+  });
+
+  it('throws an error when the signing key is missing', () => {
+    function verify() {
+      verifyWebhook({signature: 'foo', created: 1}, '');
+    }
+    expect(verify).toThrowError(VerificationError);
+    expect(verify).toThrowError('Signing key was not provided.');
+  });
+
+  it('throws an error when the signing doesn\'t match', () => {
+    function verify() {
+      verifyWebhook({signature: 'foo', created: 1}, 'bar');
+    }
+    expect(verify).toThrowError(VerificationError);
+    expect(verify).toThrowError('Signature mismatch.');
+  });
+
+  it('throws an error when the request is too old', () => {
+    function verify() {
+      verifyWebhook({
+        // crypto.createHmac('sha256', 'foobar123').update(JSON.stringify({ created: 1 })).digest('hex')
+        signature: '2ff2f86fb32dc5c5478749e0173affa2d450436e1eaff7e96f0c8db3a5935264',
+        created: 1,
+      }, 'foobar123');
+    }
+    expect(verify).toThrowError(VerificationError);
+    expect(verify).toThrowError('Request was made too far in the past.');
+  });
+
+  it('verifies a valid payload', () => {
+    const originalPayload = {
+      created: +new Date() * 1000,
+    };
+    const signature = crypto.createHmac('sha256', 'foobar123')
+      .update(JSON.stringify(originalPayload))
+      .digest('hex');
+
+    expect(() => {
+      verifyWebhook({
+        ...originalPayload,
+        signature: signature,
+      }, 'foobar123');
+    }).not.toThrow();
+  });
+});
